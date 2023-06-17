d527fac0706633b0945db7fc554ecd46ae848af1
Prabhash Rai <prabhashsahaj@gmail.com>
2023-06-17 15:30:04 +0530

    adds pagination

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,23 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css']
 })
-
-export class HomePageComponent {
+export class HomePageComponent implements OnInit {
+  showData: any;
   data: any;
+  currentPage: number = 1;
+  itemsPerPage: number = 10;
+  totalPages: number = 1;
 
-  constructor(private http: HttpClient) {
-    this.fetchJsonData();
-  }
+  constructor(private http: HttpClient) {}
 
-  fetchJsonData() {
-    this.http.get('assets/mock_data.json').subscribe((response) => {
-      this.data = response;
-      console.log(this.data); // optional: log the data to the console
+  ngOnInit() {
+    this.fetchJsonData().subscribe(() => {
+      this.showData = this.getPaginatedData();
     });
   }
+
+  fetchJsonData(): Observable<void> {
+    return this.http.get('assets/mock_data.json').pipe(
+      map((response) => {
+        this.data = response;
+        this.totalPages = Math.ceil(this.data.length / this.itemsPerPage);
+        console.log(this.data);
+      })
+    );
+  }
+
+  setPage(pageNumber: number) {
+    this.currentPage = pageNumber;
+    this.showData = this.getPaginatedData();
+  }
+
+  getPaginatedData(): any[] {
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    const endIndex = startIndex + this.itemsPerPage;
+    return this.data?.slice(startIndex, endIndex);
+  }
+
+  getPageNumbers(): number[] {
+    return Array.from({ length: this.totalPages }, (_, index) => index + 1);
+  }  
 }

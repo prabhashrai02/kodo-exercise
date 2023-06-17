7312061edb00dd923d3c706665166fe712b41f20
Prabhash Rai <prabhashsahaj@gmail.com>
2023-06-17 18:48:37 +0530

    adds search function

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -6,11 +6,12 @@ import { map } from 'rxjs/operators';
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.css']
+  styleUrls: ['./home-page.component.css'],
 })
 export class HomePageComponent implements OnInit {
   showData: any;
   data: any;
+  filteredData: any;
   currentPage: number = 1;
   itemsPerPage: number = 10;
   totalPages: number = 1;
@@ -20,6 +21,8 @@ export class HomePageComponent implements OnInit {
 
   ngOnInit() {
     this.fetchJsonData().subscribe(() => {
+      this.filteredData = this.data;
+      this.totalPages = Math.ceil(this.filteredData.length / this.itemsPerPage);
       this.showData = this.getPaginatedData();
     });
   }
@@ -28,7 +31,6 @@ export class HomePageComponent implements OnInit {
     return this.http.get('assets/mock_data.json').pipe(
       map((response) => {
         this.data = response;
-        this.totalPages = Math.ceil(this.data.length / this.itemsPerPage);
       })
     );
   }
@@ -41,29 +43,73 @@ export class HomePageComponent implements OnInit {
   getPaginatedData(): any[] {
     const startIndex = (this.currentPage - 1) * this.itemsPerPage;
     const endIndex = startIndex + this.itemsPerPage;
-    return this.data?.slice(startIndex, endIndex);
+    return this.filteredData?.slice(startIndex, endIndex);
   }
 
   onSearch(query: string) {
     this.searchQuery = query;
     this.currentPage = 1;
-    this.showData = this.getPaginatedData();
-    if (this.searchQuery.trim() !== '') {
-      this.showData = this.showData.filter((item: any) =>
-        item.name.toLowerCase().includes(this.searchQuery.toLowerCase())
-      );
+  
+    if (query.startsWith('"') && query.endsWith('"')) {
+      this.filteredData = this.getExactFilteredData();
+    } else {
+      this.filteredData = this.getFilteredData();
     }
+  
+    this.totalPages = Math.ceil(this.filteredData.length / this.itemsPerPage);
+    this.showData = this.getPaginatedData();
   }
+
+  getExactFilteredData(): any[] {
+    
+    if (this.searchQuery.trim() === '') {
+      return this.data;
+    }
+    const query = this.searchQuery.toLowerCase().trim();
+    return this.data.filter(
+      (item: any) =>
+        item.name.toLowerCase().includes(query) ||
+        item.description.toLowerCase().includes(query)
+    );
+  }
+
+  getFilteredData(): any[] {
+    
+    if (this.searchQuery.trim() === '') {
+      return this.data;
+    }
+    const query = this.searchQuery.toLowerCase().trim();
+    return this.data.filter((item: any) =>
+      item.name.toLowerCase().includes(query) || item.description.toLowerCase().includes(query) ||
+      this.checkPartialMatch(item.name.toLowerCase(), query) || this.checkPartialMatch(item.description.toLowerCase(), query)
+    );
+  }
+  
+  checkPartialMatch(text: string, query: string): boolean {
+    const words = query.split(' ');
+    for (const word of words) {
+      if (!text.includes(word)) {
+        return false;
+      }
+    }
+    return true;
+  }
+  
 
   onSort(option: string) {
     if (option === 'name') {
-      this.showData = this.showData.sort((a: any, b: any) => a.name.localeCompare(b.name));
+      this.filteredData = this.filteredData.sort((a: any, b: any) =>
+        a.name.localeCompare(b.name)
+      );
     } else if (option === 'dateLastEdited') {
-      this.showData = this.showData.sort((a: any, b: any) => {
+      this.filteredData = this.filteredData.sort((a: any, b: any) => {
         const dateA = new Date(a.dateLastEdited);
         const dateB = new Date(b.dateLastEdited);
         return dateA.getTime() - dateB.getTime();
       });
     }
+    this.currentPage = 1;
+    this.totalPages = Math.ceil(this.filteredData.length / this.itemsPerPage);
+    this.showData = this.getPaginatedData();
   }
 }

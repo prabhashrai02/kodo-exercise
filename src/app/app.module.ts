999ac4bcbff102581718a2b3810eb33dd76f3d2d
Prabhash Rai <prabhashsahaj@gmail.com>
2023-06-17 10:24:33 +0530

    import http client module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,25 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FeedCardComponent } from './feed-card/feed-card.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { TabularDisplayComponent } from './tabular-display/tabular-display.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FeedCardComponent,
+    HomePageComponent,
+    ToolbarComponent,
+    TabularDisplayComponent,
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],

9669c55761dec52d3b9561a9534fb741153a2b4b
Prabhash Rai <prabhashsahaj@gmail.com>
2023-06-19 00:16:42 +0530

    handle outside click on dropdown

--- src/common/dropdown/dropdown.component.ts
+++ src/common/dropdown/dropdown.component.ts
@@ -1,9 +1,4 @@
-import {
-  Component,
-  Input,
-  Output,
-  EventEmitter,
-} from '@angular/core';
+import { Component, Input, Output, EventEmitter, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-dropdown',
@@ -26,5 +21,13 @@ export class DropdownComponent {
     this.selectedOption = option;
     this.optionSelected.emit(option);
     this.dropdownOpen = false;
+  }
+
+  @HostListener('document:click', ['$event.target'])
+  onClickOutside(target: any) {
+    const dropdownElement = document.getElementById('dropdown');
+    if (dropdownElement && !dropdownElement.contains(target)) {
+      this.dropdownOpen = false;
+    }
   }
 }
